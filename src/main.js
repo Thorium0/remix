e1a4a8de21d21f5f59fab1d75ecfb20136602b5b
Manon.icu <gemini0525@foxmail.com>
2021-04-28 10:44:01 +0800

    test

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import LazyLoad from "@/components/LazyLoad"
 Vue.component('LazyLoad',LazyLoad)
 
 new Vue({
-	router,
-	store,
-	render: (h) => h(App),
+router,
+store,
+render: (h) => h(App),
 }).$mount("#app");
