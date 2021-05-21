0b01fa4f2e5bf052af661f8975f8f3ccff60266e
Manon.icu <gemini0525@foxmail.com>
2021-05-21 20:41:06 +0800

    ci: test

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
 const path = require('path')
 const resolve = (dir) => path.join(__dirname, dir)
-
+const isProd = process.env.NODE_ENV==='production';
+console.log(isProd)
 module.exports = {
-  devServer: {
-    port: '8888'
-  },
-
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [path.resolve(__dirname, './src/assets/scss/*.scss')]
-    }
-  }
+	publicPath:isProd?'./':'',
+	devServer: {
+		port: '8888'
+	},
+	pluginOptions: {
+		'style-resources-loader': {
+			preProcessor: 'scss',
+			patterns: [path.resolve(__dirname, './src/assets/scss/*.scss')]
+		}
+	}
 }
